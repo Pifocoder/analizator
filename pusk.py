de61cb30e81d8abc6e47982277bf4eeae9289616
Dashevich <63498376+Dashevich@users.noreply.github.com>
2023-05-20 16:33:59 +0300

    Update pusk.py
    update for tg_parser
    insert urls of channels that you need to parse
    program will request phone and code(work with instructions and maybe it will work)

--- pusk.py
+++ pusk.py
@@ -1,8 +1,11 @@
 from parsers.youtube_parser import getINFO
 from parsers.vk_parser import parse_vk
+from parsers.tg_parser import parse_tg
 
 channels = ['UCOiAwKQuFMdJuK7wwDIoKpA', 'UCdubelOloxR3wzwJG9x8YqQ', 'UCm6Ojkip8Py17C7pRhR66rA',
                 'UCFzJjgVicCtFxJ5B0P_ei8A']
 data = getINFO(channels)
 groups = [-170206299, -211210115, -26323016, 453382669, -30266386, -52620949, 561960677, 565282958, -148321046, -98331381, -216321473]
 parse_vk(groups)
+urls = ['https://t.me/moscowach']
+parse_tg(urls)
